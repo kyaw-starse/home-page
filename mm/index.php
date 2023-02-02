e02a16cce022de3fd524c02aacddd432328e8cbe
kyaw-starse <101231054+kyaw-starse@users.noreply.github.com>
2023-02-03 00:30:17 +0900

    facebook

--- mm/index.php
+++ mm/index.php
@@ -232,8 +232,8 @@
                         </div>
                     </div>
                     <div class="ftFacebook">
-                        <div id="fb-root"></div>
-                        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v15.0" nonce="aNPHKgtB"></script>
+                    <div id="fb-root"></div>
+                        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v15.0" nonce="lJdOBtia"></script>
                         <div class="fb-page" data-href="https://www.facebook.com/profile.php?id=100069089106285" data-tabs="timeline" data-width="500" data-height="250" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/profile.php?id=100069089106285" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/profile.php?id=100069089106285">STAR SE 株式会社</a></blockquote></div>
                     </div>
                 </div>
