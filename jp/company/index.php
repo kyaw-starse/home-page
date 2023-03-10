292e1c0c17f2e8e499e42e6d71d70a3987228938
kyaw-starse <101231054+kyaw-starse@users.noreply.github.com>
2023-03-10 13:34:15 +0900

    company page eng/jp buttom active status change fix

--- jp/company/index.php
+++ jp/company/index.php
@@ -48,8 +48,10 @@
                             <li class="h-icon"><a href="/home-page/jp/contact/" class="mail-icon"><img src="/home-page/assets/img/common/mail_off.png" alt=""></a></li>
                             <li class="h-icon"><a href="https://www.star-se.co.jp/starseapp/public/login" class="lock-icon"><img src="/home-page/assets/img/common/lock_off.png" alt=""></a></li>
                             <li class="site-lang">
-                                <a href="/home-page/en/company/" class="lang-link langOn">EN</a>
-                                <a href="/home-page/jp/company/" class="lang-link">JP</a>
+                                <!-- <a href="/home-page/en/company/" class="lang-link langOn">EN</a>
+                                <a href="/home-page/jp/company/" class="lang-link">JP</a> -->
+                                <a href="/home-page/en/company/" class="lang-link">EN</a>
+                                <a href="/home-page/jp/company/" class="lang-link langOn">JP</a>
                             </li>
                         </ul>
                     </div>
